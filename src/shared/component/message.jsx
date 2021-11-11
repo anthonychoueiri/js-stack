213e8dafa84f1ba9f98ccdf1aa382ac23f3f1a46
Antoine El Choueiri <64024432+anthonychoueiri@users.noreply.github.com>
2021-11-11 14:52:22 +0200

    feat: add previous files

---
+++ src/shared/component/message.jsx
@@ -0,0 +1,11 @@
+// @flow
+
+import React from 'react';
+
+type Props = {
+  message: string,
+}
+
+const Message = ({ message }: Props): React$Element<any> => <p>{message}</p>;
+
+export default Message;
