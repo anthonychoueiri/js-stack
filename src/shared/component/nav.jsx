98468a6041eae075159a2987b6aac6714617614a
Antoine El Choueiri <64024432+anthonychoueiri@users.noreply.github.com>
2021-11-17 18:04:45 +0100

    refactor: move array to const

--- src/shared/component/nav.jsx
+++ src/shared/component/nav.jsx
@@ -16,27 +16,30 @@ const handleNavLinkClick = () => {
   $('.js-navbar-collapse').collapse('hide');
 };
 
-const Nav = (): React$Element<any> => (
-  <nav className="navbar navbar-dark bg-dark navbar-inverse fixed-top">
-    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <Link to={HOME_PAGE_ROUTE} className="navbar-brand">{APP_NAME}</Link>
-    <div className="js-navbar-collapse collapse navbar-collapse">
-      <ul className="navbar-nav mr-auto">
-        {[
-          { route: HOME_PAGE_ROUTE, label: 'Home' },
-          { route: HELLO_PAGE_ROUTE, label: 'Say Hello' },
-          { route: HELLO_ASYNC_PAGE_ROUTE, label: 'Say Hello Asynchronously' },
-          { route: NOT_FOUND_DEMO_PAGE_ROUTE, label: '404 Demo' },
-        ].map((link) => (
-          <li className="nav-item" key={link.route}>
-            <NavLink to={link.route} className="nav-link" activeStyle={{ color: 'white' }} exact onClick={handleNavLinkClick}>{link.label}</NavLink>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </nav>
-);
+const Nav = (): React$Element<any> => {
+  const links = [
+    { route: HOME_PAGE_ROUTE, label: 'Home' },
+    { route: HELLO_PAGE_ROUTE, label: 'Say Hello' },
+    { route: HELLO_ASYNC_PAGE_ROUTE, label: 'Say Hello Asynchronously' },
+    { route: NOT_FOUND_DEMO_PAGE_ROUTE, label: '404 Demo' },
+  ];
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-inverse fixed-top">
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <Link to={HOME_PAGE_ROUTE} className="navbar-brand">{APP_NAME}</Link>
+      <div className="js-navbar-collapse collapse navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          {links.map((link) => (
+            <li className="nav-item" key={link.route}>
+              <NavLink to={link.route} className="nav-link" activeStyle={{ color: 'white' }} exact onClick={handleNavLinkClick}>{link.label}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
